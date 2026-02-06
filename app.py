8c8935dfe546e8d482beb9431b8c670e72bd9736
SpongyCat4468 <spongycat4468@gmail.com>
2026-02-06 22:20:18 +0800

    e

--- app.py
+++ app.py
@@ -46,6 +46,9 @@ def find_latest_checkpoint(model_dir):
         return None, 0
     
     epochs = [int(f.split('_')[-1].split('.')[0]) for f in checkpoints]
+    base = glob.glob(os.path.join(model_dir, 'crnn_epoch_base.pt'))
+    if base:
+        return os.path.join(model_dir, 'crnn_epoch_base.pt'), 'base'
     latest_epoch = max(epochs)
     latest_checkpoint = os.path.join(model_dir, f'crnn_epoch_{latest_epoch}.pt')
     
